f1939193a433a8a83ce5c0a4296ce9f57e78481d
Gerson Santos <gersones@outlook.com>
2024-04-29 18:30:06 -0300

    a

---
+++ Exercicio01DosPOO this - Copia/PacoteJavaDosPOO/Pessoa.java
@@ -0,0 +1,11 @@
+
+public class Pessoa {
+	String nome = "Carlos";
+	public static void main(String[] args) {
+        
+     Pessoa(){
+	String nome = "Henrique";
+	System.out.println(nome);
+	System.out.println(this.nome);
+    }
+}}
---
+++ Exercicio01DosPOO this - Copia/Pessoa.java
@@ -0,0 +1,11 @@
+package PacoteJavaDosPOO;
+
+public class Pessoa {
+	//String nome = "Carlos";
+	public void apresentar() { 
+     	//Pessoa(){
+	//String nome = "Henrique";
+	//System.out.println(nome);
+	System.out.println("this.nome");
+    }
+}
