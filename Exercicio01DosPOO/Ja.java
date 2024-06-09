f294bc2b3d36540d4a42429b68b93dae7fe7e8aa
Gerson Santos <gersones@outlook.com>
2024-06-09 20:09:58 -0300

    a

---
+++ Exercicio01DosPOO/Ja.java
@@ -0,0 +1,21 @@
+public class Ja {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 5;
+        while (x > 10) {
+            if (x <= y ) {
+                y = y * 2;
+             } else {
+                    y = y + 1;
+
+                }
+                x++;
+            
+            
+        }
+		
+		System.out.println(x + y);
+		
+	}
+}
+
